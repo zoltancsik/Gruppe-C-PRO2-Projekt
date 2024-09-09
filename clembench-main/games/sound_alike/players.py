2509447153fb783bcba9c267d7587b29343f06de
ramandeepshub <rakaur6450@gmail.com>
2024-09-09 13:37:24 +0200

    Add Speaker Class

--- clembench-main/games/sound_alike/players.py
+++ clembench-main/games/sound_alike/players.py
@@ -1,6 +1,9 @@
+from typing import List
 from clemgame.clemgame import Player
 
 
-class SoundAlikePlayers(Player):
-    def __init__():
-        pass
+class Speaker(Player):
+    def __init__(self, model_name: str, player: str):
+        super().__init__(model_name)
+        self.player: str = player
+        self.history: List = []
