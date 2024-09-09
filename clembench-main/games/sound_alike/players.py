76ecd675667387b4dc4c93a888a28dc7a857476e
ramandeepshub <rakaur6450@gmail.com>
2024-09-09 17:53:28 +0200

    Add basic subclass Guesser of Player

--- clembench-main/games/sound_alike/players.py
+++ clembench-main/games/sound_alike/players.py
@@ -2,8 +2,13 @@ from typing import List
 from clemgame.clemgame import Player
 
 
-class Speaker(Player):
+class Guesser(Player):
     def __init__(self, model_name: str, player: str):
         super().__init__(model_name)
         self.player: str = player
         self.history: List = []
+        self.answer: str = ""
+
+    def _custom_response(self, messages) -> str:
+        last_message = messages[-1]["content"]
+        return f"My answer to {last_message} is X"
