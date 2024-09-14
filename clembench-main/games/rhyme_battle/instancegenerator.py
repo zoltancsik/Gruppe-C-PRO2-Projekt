ba7bf5b7596b1971c77a01cd1c52a976b097d3e1
ramandeepshub <rakaur645@gmail.com>
2024-09-14 23:34:21 +0200

    Merge prompt_creator.py and instancegenerator.py

--- clembench-main/games/rhyme_battle/instancegenerator.py
+++ clembench-main/games/rhyme_battle/instancegenerator.py
@@ -91,8 +91,65 @@ class RhymeBattleInstanceGenerator(GameInstanceGenerator):
         return picked_word
 
 
+class PromptGenerator():
+    def __init__(self):
+        self.prompts_created = 0
+
+    def generate_prompt(self, difficulty, prompt_message, player):
+        if difficulty == "EASY":
+            folder = "level_easy"
+        elif difficulty == "HARD":
+            folder = "level_hard"
+        elif difficulty == "CO-OP":
+            folder = "level_coop"
+        if player == 'a':
+            x = 'a'
+        else:
+            x = 'b'
+        with open(
+                f'resources/initial_prompts/{folder}/'
+                f'initial_prompt_{x}.template', 'w') as file:
+            file.write(prompt_message)
+        self.prompts_created += 1
+
+    def generator_infos(self):
+        return print(f"PROMPTS: {self.prompts_created} Initial Prompts have been created")
+
+
 if __name__ == '__main__':
+    prompt_easy_a = (
+        "Your task come up with a  word that likely rhymes with '$t_word', using phonetic rules. "
+        "Two words rhyme if they share the same vowel in the last stressed syllable and all following sounds. "
+        "Try to Provide a perfect rhyming word (exact match after the stressed vowel) or a near rhyming word (minor variations allowed)."
+        "Use phonetic transcriptions similar to those in linguistic dictionaries like CMUdict."
+        "Your have to reach $max_p points before the other player. "
+        "Your answer must follow this format: [Previous Word] -  MY GUESS: word. "
+        "Your answer can not contain special characters or any other information."
+        "Each round, you consider your partner's current points, "
+        "Your secondary objective is to trick the other player as often as possible "
+        "by using one of the following words: $wild_cards."
+        "If you use a wildcard word and the other player catches you, you will be penalized."
+        "Try not to use words that have been used before")
+
+    prompt_easy_b = (
+        "Your task is to find a word that rhymes with the word the other player said, using phonetic rules. "
+        "Two words rhyme if they share the same vowel in the last stressed syllable and all following sounds. "
+        "Try toProvide a perfect rhyming word (exact match after the stressed vowel) or a near rhyming word (minor variations allowed)."
+        "Use phonetic transcriptions similar to those in linguistic dictionaries like CMUdict."
+        "Your have to reach $max_p points before the other player. "
+        "Your answer must follow this format: [Previous Word] -   MY GUESS: word. "
+        "Your answer can not contain special characters or any other information."
+        "if you think the other player is trying to trick you "
+        "by using a word that is phonetically completely different than the last word, "
+        "call them out by answering: MY GUESS: CHEATER."
+        "Try not to use words that have been used before"
+    )
+
+    prompt_generator = PromptGenerator()
+    prompt_generator.generate_prompt("EASY", prompt_easy_a, 'a')
+    prompt_generator.generate_prompt("EASY", prompt_easy_b, 'b')
+    prompt_generator.generator_infos()
     created_episodes = RhymeBattleInstanceGenerator()
     created_episodes.generate()
-    print(f"Finished Creating Experiment with {N_EPISODES} Episodes and "
+    print(f"INSTANCES: Created Experiment with {N_EPISODES} Episodes and "
           f"{N_EPISODES*N_INSTANCES} Instances")
